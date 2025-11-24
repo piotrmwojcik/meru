713eae69a47963b9f5df08dd1fc006490b1b14a5
Piotr Wojcik <piotr.m.wojcik@gmail.com>
2025-11-25 00:37:22 +0100

    fix

--- scripts/text_traversals.py
+++ scripts/text_traversals.py
@@ -231,7 +231,7 @@ def main(_A: argparse.Namespace):
         tokenizer = Tokenizer()
 
         text_tokens = tokenizer([prompt])
-        text_feats = model.encode_text(text_tokens, project=False)
+        text_feats = model.encode_text(text_tokens, project=False)[0]
 
         interp_feats = interpolate(model, text_feats, root_feat, _A.steps)
         nn1_scores = calc_scores(model, interp_feats, text_feats_pool, has_root=True)
